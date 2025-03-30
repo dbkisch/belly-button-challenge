3960844879a5dbefaaf97e790da44c9c31fbf6cf
dbkisch <kischdeb@gmail.com>
2025-03-30 12:25:55 -0500

    built charts - still needs more formatting and to be made dynamic

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 // Build the metadata panel
 function buildMetadata(sample) {
-  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data1) => {
 
     // get the metadata field
-    let sampleMetadata = Object.values(data.metadata);
+    let sampleMetadata = Object.values(data1.metadata);
     console.log(sampleMetadata);
     // Filter the metadata for the object with the desired sample number
       
@@ -39,31 +39,69 @@ function buildMetadata(sample) {
 
 // function to build both charts
 function buildCharts(sample) {
-  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data2) => {
 
     // Get the samples field
-
+    let samples = Object.values(data2.samples);
+    console.log(samples);
 
     // Filter the samples for the object with the desired sample number
+    function selectSample(item) {
+      return item.id == sample;
+    }
+    let chosenSample = samples.filter(selectSample);
+    console.log(chosenSample);
 
-
-    // Get the otu_ids, otu_labels, and sample_values
+    // Get the otu_ids, otu_labels, and sample_values  
+    let trace1 = {
+      x: chosenSample[0].otu_ids,
+      y: chosenSample[0].sample_values,
+      mode: 'markers',
+      marker: {
+        size: chosenSample[0].sample_values
+      },
+      type: 'scatter'
+    };
 
 
     // Build a Bubble Chart
+    let data = [trace1];
 
-
+    let layout = {
+      title: {
+        text: 'Bacteria Cultures per Sample'
+      },
+      showlegend: false,
+      height: 600,
+      width: 600
+    };
+    
     // Render the Bubble Chart
-
+    Plotly.newPlot("bubble", data, layout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    let trace2 = {
+      x: chosenSample[0].sample_values,
+      y: chosenSample[0].otu_ids,
+      type: 'bar',
+      orientation: 'h'
+    };
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    let datab = [trace2];
 
+    let layoutb = {
+      title: {
+        text: 'Top 10 Bacteria Cultures Found'
+      },
+      showlegend: false,
+      height: 600,
+      width: 600
+    };
 
     // Render the Bar Chart
+    Plotly.newPlot("bar", datab, layoutb);
 
   });
 }
@@ -96,6 +134,8 @@ function init() {
       console.log(firstSample);
     // Build charts and metadata panel with the first sample
     let test = buildMetadata(firstSample);
+    let test2 = buildCharts(firstSample);
+
     
 
 
